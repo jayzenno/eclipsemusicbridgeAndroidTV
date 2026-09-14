571fbc20483e4f5d2dc0e9ce3394c6e5a4003b2b
jayzenno <jsn.ykk@gmail.com>
2026-09-14 15:19:04 +0200

    Clean up activity after adding native playback bridge

--- app/src/main/java/app/eclipse/tv/MainActivity.kt
+++ app/src/main/java/app/eclipse/tv/MainActivity.kt
@@ -10,7 +10,6 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 
 class MainActivity : Activity() {
     private lateinit var webView: WebView
@@ -19,22 +18,15 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         window.addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         window.decorView.systemUiVisibility = (
-            View.SYSTEM_UI_FLAG_FULLSCREEN or
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         )
-
         webView = WebView(this)
         setContentView(webView)
         configureWebView()
         PlaybackBridge.connect(this)
-
-        if (savedInstanceState == null) {
-            webView.loadUrl("https://eclipsemusic.app/web/")
-        } else {
-            webView.restoreState(savedInstanceState)
-        }
+        if (savedInstanceState == null) webView.loadUrl("https://eclipsemusic.app/web/")
+        else webView.restoreState(savedInstanceState)
     }
 
     private fun configureWebView() {
@@ -48,35 +40,29 @@ class MainActivity : Activity() {
         settings.builtInZoomControls = false
         settings.displayZoomControls = false
         settings.userAgentString = settings.userAgentString + " EclipseTV/1.2"
-
         CookieManager.getInstance().setAcceptCookie(true)
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
-
         webView.isFocusable = true
         webView.isFocusableInTouchMode = true
         webView.requestFocus(View.FOCUS_DOWN)
         webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean = false
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest) = false
         }
         webView.webChromeClient = WebChromeClient()
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
-            webView.goBack()
-            return true
+            webView.goBack(); return true
         }
         return super.onKeyDown(keyCode, event)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        webView.saveState(outState)
-        super.onSaveInstanceState(outState)
+        webView.saveState(outState); super.onSaveInstanceState(outState)
     }
 
     override fun onDestroy() {
-        webView.destroy()
-        PlaybackBridge.release()
-        super.onDestroy()
+        webView.destroy(); PlaybackBridge.release(); super.onDestroy()
     }
 }
