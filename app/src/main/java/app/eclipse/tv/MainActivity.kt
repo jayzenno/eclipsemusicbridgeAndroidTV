5bb1b4d6b9f44239f88d954e22267522c6086fac
jayzenno <jsn.ykk@gmail.com>
2026-09-14 13:49:49 +0200

    Add Eclipse TV WebView app

---
+++ app/src/main/java/app/eclipse/tv/MainActivity.kt
@@ -0,0 +1,54 @@
+package app.eclipse.tv
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.CookieManager
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        )
+        webView = WebView(this)
+        setContentView(webView)
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.mediaPlaybackRequiresUserGesture = false
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.userAgentString = settings.userAgentString + " EclipseTV/1.0"
+        CookieManager.getInstance().setAcceptCookie(true)
+        webView.isFocusable = true
+        webView.isFocusableInTouchMode = true
+        webView.requestFocus(View.FOCUS_DOWN)
+        webView.loadUrl("https://eclipsemusic.app/web/")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onDestroy() {
+        webView.destroy()
+        super.onDestroy()
+    }
+}
