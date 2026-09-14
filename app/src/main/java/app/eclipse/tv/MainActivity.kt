f313a0d3175b5fbb06a0e58b17fa5d75ce25ba85
jayzenno <jsn.ykk@gmail.com>
2026-09-14 14:48:22 +0200

    Fix TV WebView activity and D-pad/back handling

--- app/src/main/java/app/eclipse/tv/MainActivity.kt
+++ app/src/main/java/app/eclipse/tv/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.KeyEvent
 import android.view.View
 import android.webkit.CookieManager
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -22,21 +23,40 @@ class MainActivity : Activity() {
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         )
+
         webView = WebView(this)
         setContentView(webView)
-        webView.webViewClient = WebViewClient()
-        webView.webChromeClient = WebChromeClient()
+        configureWebView()
+
+        if (savedInstanceState == null) {
+            webView.loadUrl("https://eclipsemusic.app/web/")
+        } else {
+            webView.restoreState(savedInstanceState)
+        }
+    }
+
+    private fun configureWebView() {
         val settings = webView.settings
         settings.javaScriptEnabled = true
         settings.domStorageEnabled = true
+        settings.databaseEnabled = true
         settings.mediaPlaybackRequiresUserGesture = false
         settings.cacheMode = WebSettings.LOAD_DEFAULT
-        settings.userAgentString = settings.userAgentString + " EclipseTV/1.0"
+        settings.setSupportZoom(false)
+        settings.builtInZoomControls = false
+        settings.displayZoomControls = false
+        settings.userAgentString = settings.userAgentString + " EclipseTV/1.1"
+
         CookieManager.getInstance().setAcceptCookie(true)
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+
         webView.isFocusable = true
         webView.isFocusableInTouchMode = true
         webView.requestFocus(View.FOCUS_DOWN)
-        webView.loadUrl("https://eclipsemusic.app/web/")
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean = false
+        }
+        webView.webChromeClient = WebChromeClient()
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
@@ -45,6 +65,11 @@ class MainActivity : Activity() {
             return true
         }
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        webView.saveState(outState)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onDestroy() {
